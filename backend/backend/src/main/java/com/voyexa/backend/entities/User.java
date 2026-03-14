31985100ce62a22d48eb84f80aea5391298e9e06
RishitModi <modirishit6@gmail.com>
2026-03-14 15:22:03 +0530

    added database and tested backend functionality

---
+++ backend/backend/src/main/java/com/voyexa/backend/entities/User.java
@@ -0,0 +1,42 @@
+package com.voyexa.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.beans.ConstructorProperties;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phone_number;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+}
---
+++ backend/backend/src/main/java/com/voyexa/backend/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.voyexa.backend.repositories;
+
+import com.voyexa.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
